9839801b29f28f7649e675b8f778a17eaca179e9
Alvin Fagan <gate.em6terua@gmail.com>
2011-04-10 10:25:20 -0400

    Define node better.

--- prottools/node/node.py
+++ prottools/node/node.py
@@ -4,12 +4,18 @@
 print "from " + __file__
 
 class Point:
-    def __init__( self, new_x, new_y, new_z ):
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    def __init__( self, new_x = 0.0, new_y = 0.0, new_z = 0.0 ):
         self.x = new_x
         self.y = new_y
         self.z = new_z
 
 class Node:
+    idlabel = " ";
+    point = Point( )
+    nodes = array( [ Point() ] )
     def __init__( self, idlabel, point, nodes ):
         self.idlabel = idlabel
         self.point = point
