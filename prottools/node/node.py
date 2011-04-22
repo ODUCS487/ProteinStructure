58dab5a9de065677ac0ec3f0e007fc92c87198ff
Alvin Fagan <gate.em6terua@gmail.com>
2011-04-22 18:45:04 -0400

    Class Nodes incomplete.

--- prottools/node/node.py
+++ prottools/node/node.py
@@ -1,22 +1,44 @@
 ## @package prottools
 # @TODO: needs full header
 
-print "from " + __file__
-
-class Point:
-    x = 0.0
-    y = 0.0
-    z = 0.0
-    def __init__( self, new_x = 0.0, new_y = 0.0, new_z = 0.0 ):
-        self.x = new_x
-        self.y = new_y
-        self.z = new_z
+import random
+import string
 
 class Node:
-    idlabel = " ";
-    point = Point( )
-    nodes = array( [ Point() ] )
-    def __init__( self, idlabel, point, nodes ):
+    def __init__( self, idlabel = "", x = 0.0, y = 0.0, z = 0.0, nodes = [] ):
         self.idlabel = idlabel
-        self.point = point
+        self.x = x
+        self.y = y
+        self.z = z
         self.nodes = nodes
+
+if __name__ == '__main__':
+    my_nodes = []
+    chars = string.ascii_uppercase + string.digits
+    char_set = random.sample( chars, len( chars ) )
+    print char_set
+    limit = 100
+    limit_a = -limit
+    limit_b = limit
+    for i in range( 0, 5 ):
+        x = random.uniform( limit_a, limit_b )
+        y = random.uniform( limit_a, limit_b )
+        z = random.uniform( limit_a, limit_b )
+        label = ''.join( char_set[:4] )
+        char_set = char_set[4:]
+        my_nodes.append( Node( label, x, y, z ) )
+        
+        print my_nodes[ i ].idlabel
+        print my_nodes[ i ].x
+        print my_nodes[ i ].y
+        print my_nodes[ i ].z
+        
+    print my_nodes[ 1 ].nodes
+    
+    my_nodes[ 1 ].nodes.append( my_nodes[ 0 ] )
+    my_nodes[ 0 ].nodes.append( my_nodes[ 1 ] )
+    my_nodes[ 1 ].nodes.append( my_nodes[ 2 ] )
+    my_nodes[ 2 ].nodes.append( my_nodes[ 1 ] )
+    
+    for node in my_nodes[ 1 ].nodes:
+		print node.idlabel
